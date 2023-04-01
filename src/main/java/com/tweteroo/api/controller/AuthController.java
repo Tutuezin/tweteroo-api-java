715b8b6907f97d300a420ea68becd62c470a7b4a
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-04-01 17:36:30 -0300

    feat: create auth controller with sigup

---
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.AuthDTO;
+import com.tweteroo.api.models.SignUp;
+import com.tweteroo.api.repository.AuthRepository;
+
+@RestController
+@RequestMapping("/signup")
+
+public class AuthController {
+
+  @Autowired
+  private AuthRepository repository;
+
+  @PostMapping
+  public void signUp(@RequestBody AuthDTO req) {
+
+    repository.save(new SignUp(req));
+  }
+
+}
