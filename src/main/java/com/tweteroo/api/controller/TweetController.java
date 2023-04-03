528b6b1e94906185a30b59d4d0dba6cad7e6ff0d
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-04-03 16:58:04 -0300

    feat: create tweet controller

---
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -0,0 +1,27 @@
+package com.tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.TweetDTO;
+import com.tweteroo.api.models.Tweet;
+import com.tweteroo.api.repository.TweetRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/tweets")
+
+public class TweetController {
+  @Autowired
+  private TweetRepository repository;
+
+  @PostMapping
+  public void create(@RequestBody @Valid TweetDTO req) {
+
+    repository.save(new Tweet(req));
+  }
+}
