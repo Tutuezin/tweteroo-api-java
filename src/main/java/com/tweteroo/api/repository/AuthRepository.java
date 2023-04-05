67c1c2cebe988baaf9aae42a57fde9a8f906ae29
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-04-05 14:46:25 -0300

    feat: find by username on auth repository

--- src/main/java/com/tweteroo/api/repository/AuthRepository.java
+++ src/main/java/com/tweteroo/api/repository/AuthRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.models.SignUp;
 
 public interface AuthRepository extends JpaRepository<SignUp, Long> {
+  List<SignUp> findByUsername(String username);
 
 }
